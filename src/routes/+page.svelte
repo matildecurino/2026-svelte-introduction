32181c74017bf732427d815ae465d5f3cfcf2f27
matildecurino <matilde.curino0509@gmail.com>
2026-04-08 15:43:12 +0200

    cleanup with component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,13 @@
 <script>
-    let count = $state(0)
-    let doubled = $derived(count * 2)
+    import ClicksCounter from "$lib/components/ClicksCounter.svelte";
     console.log("hello world")
 </script>
+
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<button onclick={() => {count++}}>
-    Clicks: {count}
-</button>
-
-<span>
-    doubled is {doubled}
-</span>
+<ClicksCounter />
 
 <style>
     h1 {

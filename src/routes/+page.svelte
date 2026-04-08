8f9e85528f6bcd58bd53d08a1a93c23939e8f3b3
matildecurino <matilde.curino0509@gmail.com>
2026-04-08 15:49:41 +0200

    customize components with props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<ClicksCounter />
+<ClicksCounter initialCount = {0} /> 
+<ClicksCounter initialCount = {3} /> 
 
 <style>
     h1 {

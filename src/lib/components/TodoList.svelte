8aaa1e4ebec05f8de312fb6f45fa7e98a05df435
matildecurino <matilde.curino0509@gmail.com>
2026-04-08 17:55:29 +0200

    use #each to show a list of todos

---
+++ src/lib/components/TodoList.svelte
@@ -0,0 +1,15 @@
+<script>
+    let todos = $state([
+        { title: "Assignemnt 1", done: false},
+        { title: "Assignemnt 2", done: false},
+        { title: "Assignemnt 3", done: false}
+    ])
+</script>
+
+<h3>My Todos</h3>
+
+<ul>
+    {#each todos as todo}
+        <li>{todo.title}</li>
+    {/each}
+</ul>
